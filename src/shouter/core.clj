6b41537752eaef3d07286e2c3964a92a3755bfb3
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2013-01-03 22:29:28 -0600

    A example web app using clojure/compojure/hiccup from Heroku's dev center page

--- src/shouter/core.clj
+++ src/shouter/core.clj
@@ -1,1 +1,17 @@
-(ns shouter.core)
+(ns shouter.core
+    (:use [compojure.core :only [defroutes GET]])
+    (:require [ring.adapter.jetty :as ring])
+    (:use [hiccup.page :only [html5]]))
+
+(defn index []
+    (html5
+        [:head
+            [:title "Hello World"]]
+        [:body
+            [:div {:id "content"} "Hello World by Rafa"]]))
+
+(defroutes routes
+    (GET "/" [] (index)))
+
+(defn start []
+    (ring/run-jetty #'routes {:port 8080 :join? false}))
